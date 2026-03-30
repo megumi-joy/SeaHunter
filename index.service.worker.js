8c7a602c79f8d84dc8194ea1b38a8dc81b696d72
Megumi <megumijoy97@gmail.com>
2026-03-30 17:45:21 +0100

    feat: initialize project with Vite and add required web assets

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773964405|104089856';
+const CACHE_VERSION = '1774541200|13961797';
 /** @type {string} */
 const CACHE_PREFIX = 'Sea Hunter-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
